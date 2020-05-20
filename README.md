572a4dd181034e3a2b99ec9b692e18d8991140e7
mdsabur <mdsabur@gmail.com>
2020-05-20 16:26:16 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-utvzgp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-utvzgp)
---
+++ index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Stuff from "./pages/Stuff";
+import Contact from "./pages/Contact";
+
+import './index.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <HashRouter>
+      <div>
+      
+         <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to ="/stuff">Stuff</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+              <Route exact path="/" component={Home}/>
+            <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+
+
+      </div>
+      </HashRouter>
+      
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
